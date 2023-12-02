fe99bf4e648e52720489a3d196205501a6c2370e
sacacitas <150604886+sacacitas@users.noreply.github.com>
2023-12-02 15:00:46 +0100

    Update datos_link_unico.js

--- datos_link_unico.js
+++ datos_link_unico.js
@@ -174,8 +174,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 if (retries_front < 250 || horas_busqueda_front < 48) {
                     document.getElementById('coste_hora_buscando_texto_entero').textContent = 'No hay datos suficientes';
                 }
-                if (state_front == 'CANCELADO') {
-                    document.getElementById('boton-cancelar-link-unico').style.display = 'none';
+                if (state_front == 'BUSCANDO') {
+                    document.getElementById('boton-cancelar-link-unico').style.display = 'block';
                 }    
                 //URL administracion dinamico 
                 var backendWebOficialElement = document.getElementById('backend-web-oficial')
