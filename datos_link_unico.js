290b42a5852faec28ba691e424d01b22ef932f53
sacacitas <150604886+sacacitas@users.noreply.github.com>
2023-12-08 18:02:24 +0100

    Update datos_link_unico.js

--- datos_link_unico.js
+++ datos_link_unico.js
@@ -93,7 +93,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 // Fetching data from jsonUrl2 based on parentIdoficinaIdservicio
                 var precio_cita_front = jsonData2[parentIDofIdoficinaIdservicio] || 'ES_0_SINDATOS';
                 var precio_cita_front_euros = (precio_cita_front / 100);
-                var precio_centimos_backend = (precio_eur_cent_front / 100);
+                const precio_centimos_backend = (precio_eur_cent_front / 100);
+                 
     
                 // 
                 var limit_max_date = new Date(limit_max_front);
@@ -172,6 +173,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 document.getElementById('caducidad_busqueda').textContent = `Dentro de ${dias_caducidad_restantes} días`;
                 document.getElementById('horas_busqueda_front').textContent = horas_busqueda_front + ' h.';
                 document.getElementById('precio_cita_front').textContent = precio_centimos_backend.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+                document.getElementById('precio_cita_hay_que_pagar').textContent = precio_centimos_backend.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 });               
                 document.getElementById('coste_hora_buscando').textContent = coste_hora_buscando.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
                 
                 
