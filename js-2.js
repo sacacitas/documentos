7e57bc180795b98395da26dc4d6e86bd1d856e42
sacacitas <150604886+sacacitas@users.noreply.github.com>
2024-01-01 20:35:25 +0100

    Update js-2.js

--- js-2.js
+++ js-2.js
@@ -1,12 +1,5 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-  // Hide segunda-parte-buscador and parte-final-buscador by default using jQuery
-  document.getElementById('oficinaSelect-text').style.display = 'none';
-  document.getElementById('oficinaSelect').style.display = 'none';
-  document.getElementById('citaPreviaSelect-text').style.display = 'none';
-  document.getElementById('citaPreviaSelect').style.display = 'none';
-
-  document.getElementById('parte-final-buscador').style.display = 'none';
 
   var administracionSelect = document.getElementById('administracionSelect');
   var provinciaSelect = document.getElementById('provinciaSelect');
@@ -14,14 +7,14 @@ document.addEventListener('DOMContentLoaded', function () {
   var segundaParteBuscador = document.getElementById('segunda-parte-buscador');
   var parteFinalBuscador = document.getElementById('parte-final-buscador');
 
+  // Hide segunda-parte-buscador and parte-final-buscador by default using jQuery
+  segundaParteBuscador.style.display = 'none';
+  parteFinalBuscador.style.display = 'none';
+  
   // Function to check whether both selects are selected
   function checkSelection() {
     if (administracionSelect.value && provinciaSelect.value) {
       segundaParteBuscador.style.display = 'block'; // Show segunda-parte-buscador
-      document.getElementById('oficinaSelect-text').style.display = 'block';
-      document.getElementById('oficinaSelect').style.display = 'block';
-      document.getElementById('citaPreviaSelect-text').style.display = 'block';
-      document.getElementById('citaPreviaSelect').style.display = 'block';
     } else {
       segundaParteBuscador.style.display = 'none'; // Hide segunda-parte-buscador
     }
