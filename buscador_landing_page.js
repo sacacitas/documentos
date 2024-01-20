5b61042a3ec84b6b10befb9e28c0c15273ae3dbe
sacacitas <150604886+sacacitas@users.noreply.github.com>
2024-01-20 21:38:45 +0100

    Update buscador_landing_page.js

--- buscador_landing_page.js
+++ buscador_landing_page.js
@@ -342,7 +342,8 @@ $(document).ready(function () {
             // Apply styles to the right column (narrow-column)
             checkoutItem.find('.narrow-column').css({
                 'flex': '15%',
-                'margin': '0px 5px 0px 5px' // Adjust the percentage as needed
+                'margin': '0px 5px 0px 5px',
+                'padding': '0'
             });
 
 
