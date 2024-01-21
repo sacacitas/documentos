bcfeac848239dc5592806ffae40f0909ac1c94d0
sacacitas <150604886+sacacitas@users.noreply.github.com>
2024-01-21 19:09:20 +0100

    Update buscador_landing_page.js

--- buscador_landing_page.js
+++ buscador_landing_page.js
@@ -225,13 +225,32 @@ $(document).ready(function () {
         }
     });
     
+
+    // Event listener for the 'radio_buscador_con_oficina' element
+    radio_buscador_con_oficina.on('change', function () {
+        if (radio_buscador_con_oficina.prop('checked')) {
+            // Reset the values of the three selects when 'Con Oficina' is selected
+            select_servicio.val('').empty().append(default_select_servicio).trigger('change');
+
+            // Fetch and populate oficina data
+            fetchJsonAndPopulateOficina();
+        }
+    });
+
+
     // Event listener for the 'radio_buscador_por_provincia' element
     radio_buscador_por_provincia.on('change', function () {
         if (radio_buscador_por_provincia.prop('checked')) {
             // Reset the values of the three selects when 'Con Oficina' is selected
             select_oficina.val('').empty().append(default_select_oficina).trigger('change');
             select_servicio.val('').empty().append(default_select_servicio).trigger('change');
+
+
+            // Fetch and populate oficina data
+            fetchJsonAndPopulateOficina();
+
         }
+
     });
   
  
