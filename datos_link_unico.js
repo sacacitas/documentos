185c59ceb0951bef9fd61c3c089b371d1383b283
sacacitas <150604886+sacacitas@users.noreply.github.com>
2024-02-04 12:54:46 +0100

    Update datos_link_unico.js

--- datos_link_unico.js
+++ datos_link_unico.js
@@ -235,19 +235,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
                 
 
-                //Cambiar textos estados cola
-                if (state_front == 'RESERVADO') {
-                    botonEstadoBusqueda.text('Cita reservada, pendiente de pagar');
-                }
-                if (state_front == 'BUSCANDO') {
-                    botonEstadoBusqueda.text('Buscando cita');
-                }        
-                if (state_front == 'CANCELADO') {
-                    botonEstadoBusqueda.text('Búsqueda cancelada');
-                }            
-                if (state_front == 'ANULADO') {
-                    botonEstadoBusqueda.text('Cita reservada anulada');
-                }    
 
 
 
@@ -280,11 +267,15 @@ document.addEventListener('DOMContentLoaded', function () {
                 document.getElementById('link-cliente-correo').textContent = clienteEmail;
     
 
-
-
+                //Cambiar textos estados cola
+                if (state_front == 'RESERVADO') {
+                    botonEstadoBusqueda.text('Cita reservada, pendiente de pagar');
+                }
 
 
   
+
+                
                 // Variables de los IFs
                 var divCosteHoraBuscando = $('#div-coste-hora-buscando');
                 var divHorasBuscando = $('#div-horas-buscando');
