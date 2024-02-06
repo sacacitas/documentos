cce020d46b67ae5d43c4350b9b4bda021e43c7d2
sacacitas <150604886+sacacitas@users.noreply.github.com>
2024-02-06 21:58:45 +0100

    Update formulario-thank-you.js

--- formulario-thank-you.js
+++ formulario-thank-you.js
@@ -9,9 +9,42 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-// Check if the 'yourParameter' exists in the current URL
-var yourParameter = getParameterByName('?redirect');
-if (yourParameter) {
-    // If the parameter exists, redirect to another URL
-    window.location.href = 'https://www.sacacitas.es/operaciones/fin-tramite';
+
+const RatingValueFormulario = getParameterByName('rating-value-formulario');
+const precioCita = getParameterByName('precio-cita');
+
+
+// Push data to the data layer
+dataLayer.push({
+    'GTM-formulario-completado': 'GTM-formulario-completado-true',
+    'rating-value-formulario': RatingValueFormulario,
+    'precio-cita': precioCita,
+    });
+
+
+
+// Check if the 'redirect' parameter exists in the current URL
+var redirectParam = getParameterByName('redirect');
+if (redirectParam) {
+    // Get all parameters from the current URL
+    var urlSearchParams = new URLSearchParams(window.location.search);
+
+    // Initialize an empty array to store parameters for the redirect URL
+    var parametersForRedirect = [];
+
+    // Iterate through all parameters to include in the redirect
+    for (var pair of urlSearchParams.entries()) {
+        if (pair[0] !== 'redirect') { // Exclude 'redirect' parameter
+            parametersForRedirect.push(pair.join('='));
+        }
+    }
+
+    // Build the redirect URL using all parameters except 'redirect'
+    var redirectUrl = 'https://www.sacacitas.es/operaciones/fin-tramite';
+    if (parametersForRedirect.length > 0) {
+        redirectUrl += '?' + parametersForRedirect.join('&');
+    }
+
+    // Redirect to the new URL
+    window.location.replace(redirectUrl);
 }
