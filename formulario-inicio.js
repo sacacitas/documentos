265e02aa0108bd8a12fd9ff9d87abe673ade3e54
sacacitas <150604886+sacacitas@users.noreply.github.com>
2024-03-07 06:52:08 +0100

    Update formulario-inicio.js

--- formulario-inicio.js
+++ formulario-inicio.js
@@ -1,34 +1,134 @@
-$("#range_02").ionRangeSlider({
-    grid: true,
-    min: 2000,
-    max: 25000,
-    from: 2500,
-    prefix: "$",
-    postfix: "+",
-    hide_min_max: true,
-    decorate_both: true,
-    force_edges: true,
-    step: 50
+//Fechas por rango
+var Webflow = Webflow || [];
+Webflow.push(function () {
+	document.getElementById('fechas-max-min').flatpickr({
+		mode: "range",
+        minDate: "today",
+        altInput: true,
+        altFormat: "F j, Y",
+        dateFormat: "Y-m-d",
+  });
 });
-$("#range_date").ionRangeSlider({
-    grid: true,
-    min: 0,
-    max: 18,
-    from_min: 0.5,
-    from: 1,
-    prefix: "± ",
-    postfix: " month(s)",
-    hide_min_max: true,
-    force_edges: true,
-    step: 0.5
-});
-
-
 
 
 var Webflow = Webflow || [];
 Webflow.push(function () {
-	document.getElementsByClassName('date').flatpickr({
-		mode: "range"
+	document.getElementById('input-fecha-nacimiento').flatpickr({
+    maxDate: "today",
+    altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d",
   });
 });
+
+
+
+$(document).ready(function () {
+
+    var selectFormDocPasaporte = $('#select-pasaporte-form');
+    var selectFormDocNIE = $('#select-nie-form');
+    var selectFormDocDNI = $('#select-dni-form');
+
+
+    selectFormDocPasaporte.addClass('boton-documento-selected');
+ 
+    // Click event for botonLinkUnicoBusqueda
+    selectFormDocPasaporte.click(function () {
+        selectFormDocPasaporte.addClass('boton-documento-selected');
+        selectFormDocNIE.removeClass('boton-documento-selected');
+        selectFormDocDNI.removeClass('boton-documento-selected');
+    });                    
+    
+    // Click event for botonLinkUnicoBusqueda
+    selectFormDocNIE.click(function () {
+        selectFormDocPasaporte.removeClass('boton-documento-selected');
+        selectFormDocNIE.addClass('boton-documento-selected');
+        selectFormDocDNI.removeClass('boton-documento-selected');
+    });                    
+    
+    // Click event for botonLinkUnicoBusqueda
+    selectFormDocDNI.click(function () {
+        selectFormDocPasaporte.removeClass('boton-documento-selected');
+        selectFormDocNIE.removeClass('boton-documento-selected');
+        selectFormDocDNI.addClass('boton-documento-selected');
+    });                    
+    
+
+
+});
+
+// For the 'js-provincia' select
+var PaisesSelect = document.getElementById('input-lista-paises'); // Replace with the actual ID of your select element
+
+
+var map_paises = {
+    "Alava": "Alava",
+    "Albacete": "Albacete",
+    "Alicante": "Alicante",
+    "Almería": "Almería",
+    "Asturias": "Asturias",
+    "Avila": "Avila",
+    "Badajoz": "Badajoz",
+    "Barcelona": "Barcelona",
+    "Burgos": "Burgos",
+    "Cáceres": "Cáceres",
+    "Cádiz": "Cádiz",
+    "Cantabria": "Cantabria",
+    "Castellón": "Castellón",
+    "Ceuta": "Ceuta",
+    "Ciudad Real": "CiudadReal",
+    "Córdoba": "Córdoba",
+    "La Coruña": "LaCoruña",
+    "Cuenca": "Cuenca",
+    "Gerona": "Gerona",
+    "Granada": "Granada",
+    "Guadalajara": "Guadalajara",
+    "Guipúzcoa": "Guipúzcoa",
+    "Huelva": "Huelva",
+    "Huesca": "Huesca",
+    "Islas Baleares": "IslasBaleares",
+    "Jaén": "Jaén",
+    "León": "León",
+    "Lérida": "Lérida",
+    "Lugo": "Lugo",
+    "Madrid": "Madrid",
+    "Málaga": "Málaga",
+    "Melilla": "Melilla",
+    "Murcia": "Murcia",
+    "Navarra": "Navarra",
+    "Orense": "Orense",
+    "Palencia": "Palencia",
+    "Las Palmas": "LasPalmas",
+    "Pontevedra": "Pontevedra",
+    "La Rioja": "LaRioja",
+    "Salamanca": "Salamanca",
+    "Segovia": "Segovia",
+    "Sevilla": "Sevilla",
+    "Soria": "Soria",
+    "Tarragona": "Tarragona",
+    "Santa Cruz De Tenerife": "SantaCruzDeTenerife",
+    "Teruel": "Teruel",
+    "Toledo": "Toledo",
+    "Valencia": "Valencia",
+    "Valladolid": "Valladolid",
+    "Vizcaya": "Vizcaya",
+    "Zamora": "Zamora",
+    "Zaragoza": "Zaragoza"
+  }
+
+
+  // Add default placeholder option
+  var defaultOption = document.createElement('option');
+  defaultOption.value = ''; // Set the value to an empty string or a value that is not present in the array
+  defaultOption.text = 'Indica tu nacionalidad';
+  defaultOption.disabled = true;
+  defaultOption.selected = true; // Make this option selected by default
+  PaisesSelect.add(defaultOption);
+
+  // Populate provincia select options
+  Object.entries(map_paises).forEach(([user_text, api_key_call]) => {
+    var optionElement = document.createElement('option');
+    optionElement.value = api_key_call;
+    optionElement.text = user_text;
+    PaisesSelect.add(optionElement);
+  });
