8af80c4e2c7a6eb590ecd5ff349919b879fc9432
Anton Burda Kivachuk <86551705+AntonBurda@users.noreply.github.com>
2024-09-17 16:56:22 +0200

    Set Tolgee

--- web-contactar.js
+++ web-contactar.js
@@ -12,133 +12,141 @@ var TEXTOS_API = {
     'js-contact-selector-placeholdertext': 'Máximo 1000 caracteres',
     'contact-button-1': 'Enviar',
 };
+//--> Set functionality for language translation
+var { Tolgee, BackendFetch } = window['@tolgee/web'];
 
+var host = window.location.host //es.sacacitas.com
+var subdomain = host.split('.')[0] //es
+var tolgee_instance = Tolgee()
+    .use(BackendFetch({ prefix: "https://documentos.sacacitas.com/lang" }))
+    .init({
+        language: subdomain
+    })
 
-
-// Check if tolgee_instance is initialized
-if (window['tolgee_instance']) {
-    console.log('tolgee_instance found, starting translation...');
-
-    // Iterate over TEXTOS_API and replace values with translations
-    for (const [key, value] of Object.entries(TEXTOS_API)) {
-        const translation = window['tolgee_instance'].t(key, `${TEXTOS_API[key]} {{${key}}}`);
-        TEXTOS_API[key] = translation;
-        console.log(`Translated key: ${key} to: ${translation}`);
-    }
-} else {
-    console.error('tolgee_instance is not initialized');
-}
-
-
-//Default placegolder text
-$('#contact-message').attr('placeholder', TEXTOS_API['js-contact-selector-placeholdertext']);
-$('#formulario-boton-finalizar').val(TEXTOS_API['contact-button-1']);
 
 
 
 
 $(document).ready(function () {
+    tolgee_instance.run().then(() => {
 
-    // Hide and show the divs
-    $('#div-contactar-datoscontactar').hide();
-    $('#div-contactar-slider').show();
-
-    // Add options to the select element
-    $('#contact-select-problem').append(`
-        <option value="NoSearchResults">${TEXTOS_API['js-contact-select-problem-0']}</option>
-        <option value="ImprovementSuggestions">${TEXTOS_API['js-contact-select-problem-1']}</option>
-        <option value="ServiceRequest">${TEXTOS_API['js-contact-select-problem-2']}</option>
-        <option value="SearchIssues">${TEXTOS_API['js-contact-select-problem-3']}</option>
-        <option value="AppointmentProblem">${TEXTOS_API['js-contact-select-problem-4']}</option>
-        <option value="ErrorGeneral">${TEXTOS_API['js-contact-select-problem-5']}</option>
-        <option value="JoinUs">${TEXTOS_API['js-contact-select-problem-6']}</option>
-        <option value="Other">${TEXTOS_API['js-contact-select-problem-7']}</option>
-    `);
+        if (subdomain === "es" || subdomain === "sacacitas") {
+            console.log("default lang ES no auto localised")
+        } else {
+            for (const [key, value] of Object.entries(TEXTOS_API)) {
+                const translation = tolgee_instance.t(key, `${TEXTOS_API[key]} {{${key}}}`);
+                TEXTOS_API[key] = translation;
+            }
+        }
+        //Default placegolder text
+        $('#contact-message').attr('placeholder', TEXTOS_API['js-contact-selector-placeholdertext']);
+        $('#formulario-boton-finalizar').val(TEXTOS_API['contact-button-1']);
 
 
 
 
-    // Intercept the form submission
-    $('#formulario-boton-finalizar').click(function (e) {
-        e.preventDefault(); // Prevent default form submission behavior
-
-        $('#gif-error-boton-finalizar-2').hide();
-
-        // Validation checks
-        var email = $('#contact-email').val();
-        var message = $('#contact-message').val();
-        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Simple email validation regex
-
-        if ($('#contact-name').val() &&
-            email &&
-            emailRegex.test(email) &&
-            $('#contact-select-problem').val() &&
-            message &&
-            message.length <= 1000) {
-
-            $('#gif-cargando-boton-finalizar-2').show();
-            $('#formulario-boton-finalizar').hide();
-
-            // Collect all form data
-            var formData = {
-                cliente_nombre: $('#contact-name').val(),
-                cliente_email: $('#contact-email').val(),
-                problem_value: $('#contact-select-problem').val(),
-                contact_reason: $('#contact-select-problem option:selected').text(), // Get the selected option's text
-                contact_message: $('#contact-message').val(),
-            };
 
 
-            // Send the POST request
-            $.post('https://n8n.sacacitas.com/webhook/contact', formData)
-                .done(function (response) {
-                    // Handle success
-                    console.log('Form successfully submitted:', response);
-                    // Optionally, show a success message to the user
+        // Hide and show the divs
+        $('#div-contactar-datoscontactar').hide();
+        $('#div-contactar-slider').show();
 
-                    // Hide and show the divs
-                    $('#gif-cargando-boton-finalizar-2').hide();
-                    $('#gif-success-boton-finalizar-2').show();
-                })
-                .fail(function (error) {
-                    // Handle error
-                    console.log('Form submission failed:', error);
-                    // Optionally, show an error message to the user
-
-                    $('#gif-cargando-boton-finalizar-2').hide();
-                    $('#gif-error-boton-finalizar-2').show();
+        // Add options to the select element
+        $('#contact-select-problem').append(`
+            <option value="NoSearchResults">${TEXTOS_API['js-contact-select-problem-0']}</option>
+            <option value="ImprovementSuggestions">${TEXTOS_API['js-contact-select-problem-1']}</option>
+            <option value="ServiceRequest">${TEXTOS_API['js-contact-select-problem-2']}</option>
+            <option value="SearchIssues">${TEXTOS_API['js-contact-select-problem-3']}</option>
+            <option value="AppointmentProblem">${TEXTOS_API['js-contact-select-problem-4']}</option>
+            <option value="ErrorGeneral">${TEXTOS_API['js-contact-select-problem-5']}</option>
+            <option value="JoinUs">${TEXTOS_API['js-contact-select-problem-6']}</option>
+            <option value="Other">${TEXTOS_API['js-contact-select-problem-7']}</option>
+        `);
 
 
-                    //if error call to webhook
-                    $.ajax({
-                        url: "https://n8n.sacacitas.com/webhook/error-alerts",
-                        type: "POST",
-                        contentType: "application/json", // Specify content type as JSON
-                        dataType: 'json',
-                        data: JSON.stringify({
-                            LocalisationError: "web_contactar-send-form",
-                            Extrainfo: "Llamar a n8n para enviar el formulario", // Add extra text or data
-                            errorCode: 500 // Example of sending an additional error code
-                        }),
-                        success: function (response) {
-                            console.log("Success:", response);
-                        },
-                        error: function (jqXHR, textStatus, errorThrown) {
-                            console.error("Error:", errorThrown);
-                        }
+
+
+        // Intercept the form submission
+        $('#formulario-boton-finalizar').click(function (e) {
+            e.preventDefault(); // Prevent default form submission behavior
+
+            $('#gif-error-boton-finalizar-2').hide();
+
+            // Validation checks
+            var email = $('#contact-email').val();
+            var message = $('#contact-message').val();
+            var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Simple email validation regex
+
+            if ($('#contact-name').val() &&
+                email &&
+                emailRegex.test(email) &&
+                $('#contact-select-problem').val() &&
+                message &&
+                message.length <= 1000) {
+
+                $('#gif-cargando-boton-finalizar-2').show();
+                $('#formulario-boton-finalizar').hide();
+
+                // Collect all form data
+                var formData = {
+                    cliente_nombre: $('#contact-name').val(),
+                    cliente_email: $('#contact-email').val(),
+                    problem_value: $('#contact-select-problem').val(),
+                    contact_reason: $('#contact-select-problem option:selected').text(), // Get the selected option's text
+                    contact_message: $('#contact-message').val(),
+                };
+
+
+                // Send the POST request
+                $.post('https://n8n.sacacitas.com/webhook/contact', formData)
+                    .done(function (response) {
+                        // Handle success
+                        console.log('Form successfully submitted:', response);
+                        // Optionally, show a success message to the user
+
+                        // Hide and show the divs
+                        $('#gif-cargando-boton-finalizar-2').hide();
+                        $('#gif-success-boton-finalizar-2').show();
+                    })
+                    .fail(function (error) {
+                        // Handle error
+                        console.log('Form submission failed:', error);
+                        // Optionally, show an error message to the user
+
+                        $('#gif-cargando-boton-finalizar-2').hide();
+                        $('#gif-error-boton-finalizar-2').show();
+
+
+                        //if error call to webhook
+                        $.ajax({
+                            url: "https://n8n.sacacitas.com/webhook/error-alerts",
+                            type: "POST",
+                            contentType: "application/json", // Specify content type as JSON
+                            dataType: 'json',
+                            data: JSON.stringify({
+                                LocalisationError: "web_contactar-send-form",
+                                Extrainfo: "Llamar a n8n para enviar el formulario", // Add extra text or data
+                                errorCode: 500 // Example of sending an additional error code
+                            }),
+                            success: function (response) {
+                                console.log("Success:", response);
+                            },
+                            error: function (jqXHR, textStatus, errorThrown) {
+                                console.error("Error:", errorThrown);
+                            }
+                        });
+
+
                     });
 
+            } else {
+                $('#gif-error-boton-finalizar-2').show(); // Show error gif if validation fails
+            }
+        });
 
-                });
-
-        } else {
-            $('#gif-error-boton-finalizar-2').show(); // Show error gif if validation fails
-        }
+        // Optionally, deactivate the original form submission behavior
+        $('#email-form').submit(function (e) {
+            e.preventDefault(); // Prevent the original Webflow form submission
+        });
     });
-
-    // Optionally, deactivate the original form submission behavior
-    $('#email-form').submit(function (e) {
-        e.preventDefault(); // Prevent the original Webflow form submission
-    });
-
 });
