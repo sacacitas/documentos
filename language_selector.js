3e58d9bf5e1247eb766c2cef69671bff929473f1
Anton Burda Kivachuk <86551705+AntonBurda@users.noreply.github.com>
2024-09-25 20:56:28 +0200

    make it functional

--- language_selector.js
+++ language_selector.js
@@ -1,15 +1,52 @@
-document.addEventListener('DOMContentLoaded', function() {
+$(document).ready(function () {
+    // Function to set a cookie
+    function setCookie(name, value, days) {
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    }
 
-    const currentLinkSwitcher2 = document.querySelectorAll('[selector="dropdown2"].w--current');
-  
-    currentLinkSwitcher2.forEach(link => {
-      const closestLocalItem2 = link.closest('.ouiflow-2_list-item');
-      if (closestLocalItem2) {
-        closestLocalItem2.style.display = 'none';
-      }
+    console.log(getCookie('SC_lang'));
+    // Function to get a cookie value (optional if needed)
+    function getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
+
+    // Attach click event to all anchor tags with the attribute 'listenerlanguageitems'
+    $('a[listenerlanguageitems]').on('click', function (event) {
+        // Prevent the default link behavior
+        event.preventDefault();
+
+        // Get the value of the listenerlanguageitems attribute
+        var SC_lang = $(this).attr('listenerlanguageitems');
+
+        // Set the selected language in a cookie
+        setCookie('SC_lang', SC_lang, 365);
+
+        // Get the current path and query string
+        var currentPath = window.location.pathname + window.location.search;
+
+        // Construct the URL using the language attribute value as the subdomain
+        var url = 'https://' + SC_lang + '.sacacitas.com' + currentPath;
+
+        // Redirect to the constructed URL
+        window.location.href = url;
     });
-  
-  });
-  
 
-  
+    // SWITCHER-4 logic to hide the current language link
+    const currentLinksSwitcher4 = $('a[listenerlanguageitems].w--current');
+    currentLinksSwitcher4.each(function () {
+        $(this).hide();
+    });
+});
