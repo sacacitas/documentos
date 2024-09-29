59b46adf67b1ee70048b4f307e387d25b99b1fe5
Anton Burda Kivachuk <86551705+AntonBurda@users.noreply.github.com>
2024-09-29 20:31:04 +0200

    change to vanilla JS

--- language_selector.js
+++ language_selector.js
@@ -1,4 +1,6 @@
-$(document).ready(function () {
+// Function to run after the DOM has fully loaded
+document.addEventListener('DOMContentLoaded', function () {
+
     // Function to set a cookie for the parent domain (accessible from all subdomains)
     function setCookie(name, value, days) {
         var expires = "";
@@ -16,8 +18,7 @@ $(document).ready(function () {
         var nameEQ = name + "=";
         var ca = document.cookie.split(';');
         for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+            var c = ca[i].trim();
             if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
         }
         return null;
@@ -26,30 +27,34 @@ $(document).ready(function () {
     // Check if a language cookie already exists
     var currentLanguage = getCookie('SC_lang');
     var currentSubdomain = window.location.hostname.split('.')[0];  // Get the subdomain
+
     // Only set the cookie to the current subdomain if no language cookie exists
     if (!currentLanguage) {
         setCookie('SC_lang', currentSubdomain, 365);
     }
 
     // Attach click event to all anchor tags with the attribute 'ListenerLanguageItems'
-    $('a[ListenerLanguageItems]').on('click', function (event) {
-        // Prevent the default link behavior
-        event.preventDefault();
+    document.querySelectorAll('a[ListenerLanguageItems]').forEach(function (anchor) {
+        anchor.addEventListener('click', function (event) {
+            // Prevent the default link behavior
+            event.preventDefault();
 
-        // Get the value of the listenerlanguageitems attribute
-        var SC_lang = $(this).attr('ListenerLanguageItems');
-        console.log("Selected language (SC_lang):", SC_lang);
+            // Get the value of the ListenerLanguageItems attribute
+            var SC_lang = this.getAttribute('ListenerLanguageItems');
+            console.log("Selected language (SC_lang):", SC_lang);
 
-        // Update the language cookie with the new selected language
-        setCookie('SC_lang', SC_lang, 365);
+            // Update the language cookie with the new selected language
+            setCookie('SC_lang', SC_lang, 365);
 
-        // Get the current path and query string
-        var currentPath = window.location.pathname + window.location.search;
+            // Get the current path and query string
+            var currentPath = window.location.pathname + window.location.search;
 
-        // Construct the URL using the language attribute value as the subdomain
-        var url = 'https://' + SC_lang + '.sacacitas.com' + currentPath;
+            // Construct the URL using the language attribute value as the subdomain
+            var url = 'https://' + SC_lang + '.sacacitas.com' + currentPath;
 
-        // Redirect to the constructed URL
-        window.location.href = url;
+            // Redirect to the constructed URL
+            window.location.href = url;
+        });
     });
+
 });
