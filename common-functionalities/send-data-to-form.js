5ee7bf632c3d1ab6959d0292855c5a69014519b9
Anton Burda <86551705+AntonBurda@users.noreply.github.com>
2025-02-02 10:03:33 +0100

    Update send-data-to-form.js

--- common-functionalities/send-data-to-form.js
+++ common-functionalities/send-data-to-form.js
@@ -1,4 +1,7 @@
 $("[go-to-form-services]").click(function () {
-    // Redirect to the form page
-    window.location.href = `https://sacacitas.webflow.io/services/form`;
+    // Get the last path segment of the current page URL
+    let lastPathSegment = window.location.pathname.split('/').filter(Boolean).pop();
+
+    // Redirect to the form page with the last path segment as a parameter
+    window.location.href = `https://sacacitas.webflow.io/services/form?ref=${encodeURIComponent(lastPathSegment)}`;
 });
